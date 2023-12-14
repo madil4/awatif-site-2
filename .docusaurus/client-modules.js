5e3cc076b391d2f14e9b1e2e3ef743312270953d
Mohamed Adil <me@mohamedadil.com>
2023-12-14 21:58:16 +0100

    install docusarusus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/mohamed/Documents/awatif-site-2/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/mohamed/Documents/awatif-site-2/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/mohamed/Documents/awatif-site-2/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/mohamed/Documents/awatif-site-2/src/css/custom.css"),
+];
